e922532ce2b4a06d01c9b6ffc8ba4e9fadaad093
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-11-23 20:23:25 +0100

    Added missing back end module table entry

--- contao-module/config/config.php
+++ contao-module/config/config.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Back end modules
+ */
+$GLOBALS['BE_MOD']['design']['themes']['tables'][] = 'tl_css_class_replacer';
+
+/**
+ * Hooks
+ */
 $GLOBALS['TL_HOOKS']['outputFrontendTemplate'][] = array(
     'Toflar\Contao\CssClassReplacer\Listener',
     'replaceCssClasses'
