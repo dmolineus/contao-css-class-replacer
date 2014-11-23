abdd29f5072b33f4a2c09e600c32c306f0090f62
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-11-23 21:11:14 +0100

    Added a nice icon

--- contao-module/dca/tl_theme.php
+++ contao-module/dca/tl_theme.php
@@ -6,5 +6,5 @@
 $GLOBALS['TL_DCA']['tl_theme']['list']['operations']['css-class-replacer'] = array(
     'label'               => &$GLOBALS['TL_LANG']['tl_theme']['css-class-replacer'],
     'href'                => 'table=tl_css_class_replacer',
-    'icon'                => 'css.gif'
+    'icon'                => 'system/modules/css-class-replacer/assets/css-class-replacer.png'
 );
