a62b53e7a5f7d24bf58ead3ab66c539dac14d0f6
David Molineus <mail@netzmacht.de>
2014-12-10 07:52:43 +0100

    implement ClassAttributeRule

---
+++ src/Rule/ClassAttributeRule.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Toflar\Contao\CssClassReplacer\Rule;
+
+class ClassAttributeRule extends AbstractRule
+{
+    /**
+     * Apply Rule filters.
+     *
+     * @param \DomElement $node
+     *
+     * @return mixed
+     */
+    public function apply(\DomElement $node)
+    {
+        $attr       = $node->attributes;
+        $classNode  = $attr->getNamedItem('class');
+        $classValue = $this->applyFilter($classNode ? $classNode->nodeValue : '');
+
+        // Replace if it already exists
+        if ($classNode) {
+            $classNode->nodeValue = $classValue;
+        } else {
+            // Otherwise add
+            $node->setAttribute('class', $classValue);
+        }
+    }
+
+    /**
+     * Apply defined filters.
+     *
+     * @param string $cssClass Css class value.
+     *
+     * @return string
+     */
+    private function applyFilter($cssClass)
+    {
+        foreach ($this->getFilters() as $filter) {
+            $cssClass = $filter($cssClass);
+        }
+
+        return $cssClass;
+    }
+}
