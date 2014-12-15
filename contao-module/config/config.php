8b10c129d11f4ea10639f2d2958dd5e562245829
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-12-15 18:12:48 +0100

    Added support for easy_themes

--- contao-module/config/config.php
+++ contao-module/config/config.php
@@ -27,3 +27,11 @@ $GLOBALS['TL_MODELS']['tl_css_class_replacer'] = 'Toflar\Contao\CssClassReplacer
  * Maintenance
  */
 $GLOBALS['TL_MAINTENANCE'][] = 'Toflar\Contao\CssClassReplacer\Maintenance\RuleUpdate';
+
+/**
+ * easy_themes support
+ */
+$GLOBALS['TL_EASY_THEMES_MODULES']['css-class-replacer'] = array(
+    'href_fragment' => 'table=tl_css_class_replacer',
+    'icon'          => 'system/modules/css-class-replacer/assets/css-class-replacer.png'
+);
